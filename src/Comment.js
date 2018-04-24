f84357b9119aee450ad9e10b560c69f26785c929
Małgorzata Kapuścińska <m_kapuscinska1@o2.pl>
2018-04-24 17:28:03 +0200

    Add CommentsList

--- src/Comment.js
+++ src/Comment.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Comment = ({text, votes}) => <li> {text} <span>votes</span</li>;
+const Comment = ({text, votes}) => <li> {text} <span>votes: {votes}</span</li>;
 
 export default Comment;
